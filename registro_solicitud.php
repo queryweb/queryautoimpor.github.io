0b128b99b469cface380504bcd726fcddf12f8df
queryweb <155488753+queryweb@users.noreply.github.com>
2024-01-06 13:24:30 -0400

    Add files via upload

---
+++ registro_solicitud.php
@@ -0,0 +1,89 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet"href="registro_solicitud.css? 20258">
+  <title>Queryautoimport</title>
+</head>
+<body>
+
+    <title>formulario de solicitud</title>
+</head>
+<body>
+    <div>
+<h1 class ="encabesado">FORMULARIO DE SOLICITUD <br>
+</div>
+
+
+    
+<h2 id class="correo">
+      Whatsapp: 849-751-2803 <br>
+        Crreo: queryautoimport@gmail.com <br>
+        </h2>
+          <main>
+
+<div id ="bancos">
+
+<img src="img/br.png" alt="unaimagen "   width ="150px" >
+<img src="img/bP.png" alt="unaimagen "   width ="150px" >
+<img src="img/bhd.png" alt="unaimagen "   width ="200px" >
+</div>
+
+
+ <MAIN>
+  <div class="img"></div>
+ </MAIN>
+
+<div>
+<a href="http://localhost/Venta_de_Vehiculos/Vehiculos.PHP">BOLVER ATRA</a>
+</div>
+<h2>
+  LLENAR SOLICITUD
+</h2>
+
+<form action="https://formsubmit.co/querycalzados@gmail.com" method="POST"> 
+
+<div id = "texto">  
+<LAbel for="Nombre">Nombres</LAbel>
+<input type="text" name = "nombres" id ="nombres"required > 
+
+<LAbel for="apellidos">Apellidos</LAbel>
+<input type="text" name = "apellidos" id ="apellidos">
+
+<LAbel for="cedula">Cedula</LAbel>
+<input type="text" name = "cedula" id ="cedula"> 
+
+<LAbel for="email">Email</LAbel>
+<input type="email" name = "email" id ="email"> 
+
+<LAbel for="telefono">Telefono</LAbel>
+<input type="text" name = "telefono" id ="telefono"> 
+</div>
+
+
+<div id = "texto1">  
+<LAbel>Ingreso</LAbel>
+<input type="text" name = "ingreso" required > 
+<LAbel>Direccion</LAbel>
+<input type="text" name = "direccion">
+<LAbel>Marca</LAbel>
+<input type="text" name = "marca"> 
+<LAbel>Año</LAbel>
+<input type="text" name = "ano"> 
+
+</div>
+
+<input class ="btn" type="submit" name="enviar" value="ENVIAR SOLICITUD " > <br>
+<input type="hidden" name="_captcha" value="false">
+<input type="hidden" name="_next"  values="registro_solicitud.php">
+
+
+</form>
+
+
+
+<!-- comentario-->
+</body>
+</html>
